bc7abd39d081f2613f90cc1f67ce7772be9e2a70
Yogesh Mishra <132771057+yogeshmishra3@users.noreply.github.com>
2025-11-26 15:12:16 +0530

    Change API base URL to production endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://backend-hms-nu.vercel.app//api'
 });
 
 API.interceptors.request.use((config) => {
